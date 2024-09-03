a958d0f984f80a523d0f951b820604245e3c5ade
nagasrinu289 <nagasrinu289@gmail.com>
2024-09-03 10:09:06 +0530

    added frontend dir

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,18 +3,17 @@ from .database import engine, Base
 from .routers import books
 from fastapi.middleware.cors import CORSMiddleware
 
-# Initialize the database
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins, can be adjusted for production
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"],  # Allows all HTTP methods
-    allow_headers=["*"],  # Allows all headers
+    allow_methods=["*"],  
+    allow_headers=["*"], 
 )
 
 @app.get('/')
